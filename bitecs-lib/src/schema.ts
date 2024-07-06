38d5036b5e82ddc44b79e590a8c9aec6e02efa3c
Cory Watson <cryo.warden@gmail.com>
2024-07-06 05:39:14 -0700

    Remove outdated bitecs-lib design

--- bitecs-lib/src/schema.ts
+++
@@ -1,9 +0,0 @@
-import { AsTsType, Types } from "./types";
-
-export type Schema = { [key: string]: Types };
-
-export type SchemaRecord = Readonly<{ [key: string]: Schema }>;
-
-export type AsTsSchema<TSchema extends Schema> = {
-  [key in keyof TSchema]: AsTsType<TSchema[key]>;
-};
--- bitecs-lib/test/componentRecord.ts
+++
@@ -1,9 +0,0 @@
-import { asComponentRecord } from "../src";
-import { ActivityQueue, Player, Position, RandomFlier } from "./schema";
-
-export const componentRecord = asComponentRecord({
-  Player,
-  Position,
-  RandomFlier,
-  ActivityQueue,
-} as const);
