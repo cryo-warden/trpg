63ab3085b9f90033ec9cd11d8ebe6a56a9bd77f6
Cory Watson <cryo.warden@gmail.com>
2024-07-21 22:16:20 -0700

    Allow world access within system actions

--- bitecs-helpers/src/system.ts
+++ bitecs-helpers/src/system.ts
@@ -5,21 +5,17 @@ export type ResourceSystem<R = {}, T extends IWorld = IWorld> = (
   resource: R
 ) => System<[], T>;
 
-type ResourceAction<R> = (resource: R) => (id: EntityId) => void;
-
 type ActionResourceSystemSpec<R, T extends IWorld> = {
   query: Query<T>;
-  action: ResourceAction<R>;
+  action: (resource: R) => (id: EntityId, value: T) => void;
 };
-
-type CreateResourceSystemOfQuery = <R, T extends IWorld>(
-  spec: ActionResourceSystemSpec<R, T>
-) => ResourceSystem<R, T>;
 
 type CrossActionResourceSystemSpec<R, T extends IWorld> = {
   distinct?: boolean;
   queries: [Query<T>, Query<T>];
-  crossAction: (resource: R) => (lhsId: EntityId, rhsId: EntityId) => void;
+  crossAction: (
+    resource: R
+  ) => (lhsId: EntityId, rhsId: EntityId, value: T) => void;
 };
 
 export type ResourceSystemSpec<R = {}, T extends IWorld = IWorld> =
@@ -28,11 +24,9 @@ export type ResourceSystemSpec<R = {}, T extends IWorld = IWorld> =
   | CrossActionResourceSystemSpec<R, T>
   | readonly ResourceSystemSpec<R, T>[];
 
-type CreateResourceSystemOf2Queries = <R, T extends IWorld>(
-  spec: CrossActionResourceSystemSpec<R, T>
-) => ResourceSystem<R, T>;
-
-const createResourceSystemOfQuery: CreateResourceSystemOfQuery =
+const createResourceSystemOfQuery: <R, T extends IWorld>(
+  spec: ActionResourceSystemSpec<R, T>
+) => ResourceSystem<R, T> =
   ({ query, action }) =>
   (resource) => {
     const resourcedAction = action(resource);
@@ -40,13 +34,15 @@ const createResourceSystemOfQuery: CreateResourceSystemOfQuery =
     return (value) => {
       const ids = query(value) as EntityId[];
       for (let i = 0; i < ids.length; ++i) {
-        resourcedAction(ids[i]);
+        resourcedAction(ids[i], value);
       }
       return value;
     };
   };
 
-const createResourceSystemOf2Queries: CreateResourceSystemOf2Queries =
+const createResourceSystemOf2Queries: <R, T extends IWorld>(
+  spec: CrossActionResourceSystemSpec<R, T>
+) => ResourceSystem<R, T> =
   ({ distinct, queries: [lhsQuery, rhsQuery], crossAction }) =>
   (resource) => {
     const resourceCrossAction = crossAction(resource);
@@ -59,7 +55,7 @@ const createResourceSystemOf2Queries: CreateResourceSystemOf2Queries =
         for (let i = 0; i < lhsIds.length; ++i) {
           for (let j = 0; j < rhsIds.length; ++j) {
             if (lhsIds[i] !== rhsIds[j]) {
-              resourceCrossAction(lhsIds[i], rhsIds[j]);
+              resourceCrossAction(lhsIds[i], rhsIds[j], value);
             }
           }
         }
@@ -72,20 +68,16 @@ const createResourceSystemOf2Queries: CreateResourceSystemOf2Queries =
       const rhsIds = rhsQuery(value) as EntityId[];
       for (let i = 0; i < lhsIds.length; ++i) {
         for (let j = 0; j < rhsIds.length; ++j) {
-          resourceCrossAction(lhsIds[i], rhsIds[j]);
+          resourceCrossAction(lhsIds[i], rhsIds[j], value);
         }
       }
       return value;
     };
   };
 
-type CreateResourceSystemOfPipeline = <R, T extends IWorld>(
+const createResourceSystemOfPipeline: <R, T extends IWorld>(
   specs: readonly ResourceSystemSpec<R, T>[]
-) => ResourceSystem<R, T>;
-
-const createResourceSystemOfPipeline: CreateResourceSystemOfPipeline = (
-  specs
-) => {
+) => ResourceSystem<R, T> = (specs) => {
   const systems = specs.map(internalCreateResourceSystem);
   return (resource) => {
     const resourcedSystems = systems.map((system) => system(resource));
@@ -99,26 +91,18 @@ const createResourceSystemOfPipeline: CreateResourceSystemOfPipeline = (
   };
 };
 
-// NOTE: Why are there two different types for CreateResourceSystem?
-// There seems to be a TypeScript inference limitation.
-// We want users to be able to specify an array argument where we can
-// infer the resource type R as the intersection of all elements'
-// resource types.
-// However, we need a separate internal type to gracefully handle recursion.
-// The extra inferential power seems to break the recursive case.
-
-type CreateResourceSystem = <
-  S extends ResourceSystemSpec<any, T>,
-  T extends IWorld
->(
-  s: S
-) => ResourceSystem<S extends ResourceSystemSpec<infer R, T> ? R : never, T>;
-
 /**
  * Create a ResourceSystem, a function which takes a specific type as a
  * resource and returns a bitecs System.
  */
-export const createResourceSystem: CreateResourceSystem = (spec) => {
+export const createResourceSystem: <
+  S extends ResourceSystemSpec<any, T>,
+  T extends IWorld
+>(
+  s: S
+) => ResourceSystem<S extends ResourceSystemSpec<infer R, T> ? R : never, T> = (
+  spec
+) => {
   if ("query" in spec) {
     return createResourceSystemOfQuery(spec);
   }
@@ -139,6 +123,14 @@ export const createResourceSystem: CreateResourceSystem = (spec) => {
     cause: spec,
   });
 };
+
+// NOTE: Why are there two different types for CreateResourceSystem?
+// There seems to be a TypeScript inference limitation.
+// We want users to be able to specify an array argument where we can
+// infer the resource type R as the intersection of all elements'
+// resource types.
+// However, we need a separate internal type to gracefully handle recursion.
+// The extra inferential power seems to break the recursive case.
 
 const internalCreateResourceSystem: <R, T extends IWorld = IWorld>(
   spec: ResourceSystemSpec<R, T>
