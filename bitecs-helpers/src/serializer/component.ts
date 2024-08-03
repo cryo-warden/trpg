eee7ad58582c1178b5d282a71928e1a5d1bb19b1
Cory Watson <cryo.warden@gmail.com>
2024-08-03 05:40:22 -0700

    Fix component serialization: nested objects/arrays

--- bitecs-helpers/src/serializer/component.ts
+++ bitecs-helpers/src/serializer/component.ts
@@ -1,35 +1,55 @@
 import { EntityId, TsComponent } from "../types";
 import { Component } from "bitecs";
 
-type ComponentSerializer = {
+export const componentSerializer = {
   serializeComponent: <TComponent extends Component>(
     component: TComponent,
     entityId: EntityId
-  ) => TsComponent<TComponent>;
+  ): TsComponent<TComponent> => {
+    const data: any = {};
+
+    for (const key of Object.keys(component)) {
+      const field: any = (component as any)[key];
+
+      if (ArrayBuffer.isView(field)) {
+        data[key] = (field as any)[entityId];
+      } else if (ArrayBuffer.isView(field[entityId])) {
+        const valueArray: any[] = [];
+        const componentArray = field[entityId] as unknown as any[];
+        for (let i = 0; i < componentArray.length; ++i) {
+          valueArray.push(componentArray[i]);
+        }
+        data[key] = valueArray;
+      } else {
+        data[key] = componentSerializer.serializeComponent(field, entityId);
+      }
+    }
+
+    return data;
+  },
   deserializeComponent: <TComponent extends Component>(
     component: TComponent,
     entityId: EntityId,
     data: TsComponent<TComponent>
-  ) => void;
-};
-
-export const componentSerializer: ComponentSerializer = {
-  serializeComponent: (component: any, entityId) => {
-    const componentData: any = {};
-
-    for (const key of Object.keys(component)) {
-      componentData[key] = component[key][entityId];
-    }
-
-    return componentData;
-  },
-  deserializeComponent: (component: any, entityId, data) => {
+  ): void => {
     for (const key of Object.keys(data) as (keyof typeof data)[]) {
       if (!(key in component)) {
         throw new Error(`Unknown field name: ${String(key)}`);
       }
 
-      component[key][entityId] = data[key];
+      const field: any = component[key];
+      const value: any = data[key];
+
+      if (ArrayBuffer.isView(field)) {
+        (field as any)[entityId] = data[key];
+      } else if (ArrayBuffer.isView(field[entityId])) {
+        const componentArray = field[entityId] as unknown as any[];
+        for (let i = 0; i < componentArray.length; ++i) {
+          componentArray[i] = value[i];
+        }
+      } else {
+        componentSerializer.deserializeComponent(field, entityId, value);
+      }
     }
   },
 };
