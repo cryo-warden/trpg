fd9d9b04060c655b03c9a2289dbfc8620225b656
Cory Watson <cryo.warden@gmail.com>
2025-03-15 01:47:06 -0700

    Juice up HPBar

--- web-trpg/src/HPBar.tsx
+++ web-trpg/src/HPBar.tsx
@@ -1,25 +1,40 @@
+import { useRef } from "react";
 import { WithEntity } from "./EntityComponent";
 import "./HPBar.css";
 
 export const HPBar = WithEntity(({ entity }) => {
+  const lastHPRatioRef = useRef(0);
+  const wasHPRisingRef = useRef(true);
+
   if (entity.hp == null || entity.mhp == null) {
     return null;
   }
 
+  const hpRatio = Math.max(0, 100 * entity.hp) / entity.mhp;
+  const isHPRising =
+    (wasHPRisingRef.current && hpRatio >= lastHPRatioRef.current) ||
+    (!wasHPRisingRef.current && hpRatio > lastHPRatioRef.current);
+  lastHPRatioRef.current = hpRatio;
+  wasHPRisingRef.current = isHPRising;
+  const cdpRatio = Math.max(0, 100 * (entity.cdp ?? 0)) / entity.mhp;
+
   return (
     <div className="HPBar">
       <div
-        className="hp"
+        className="smooth-hp"
         style={{
-          width: `${Math.max(0, 100 * entity.hp) / entity.mhp}%`,
+          width: `${hpRatio}%`,
+          transitionDuration: isHPRising ? "0.25s" : "1s",
         }}
       ></div>
       <div
-        className="cdp"
+        className="hp"
         style={{
-          width: `${Math.max(0, 100 * (entity.cdp ?? 0)) / entity.mhp}%`,
+          width: `${hpRatio}%`,
+          transitionDuration: isHPRising ? "1s" : "0.25s",
         }}
       ></div>
+      <div className="cdp" style={{ width: `${cdpRatio}%` }}></div>
     </div>
   );
 });
