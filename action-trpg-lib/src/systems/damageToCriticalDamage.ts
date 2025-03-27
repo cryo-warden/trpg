63744e8ce9cd5aa7a850c4614c1abb54247c431c
Cory Watson <cryo.warden@gmail.com>
2025-03-27 01:12:22 -0700

    Fix test

--- action-trpg-lib/src/systems/damageToCriticalDamage.ts
+++ action-trpg-lib/src/systems/damageToCriticalDamage.ts
@@ -5,13 +5,16 @@ export default createSystem((engine) => {
   const entities = engine.world.with("accumulatedDamage");
   return () => {
     for (const entity of entities) {
-      if (entity.accumulatedCriticalDamage != null) {
-        entity.accumulatedCriticalDamage += Math.max(
-          0,
-          Math.floor(
-            entity.accumulatedDamage / (entity.criticalDamageThreshold ?? 2)
-          ) - (entity.criticalDefense ?? 0)
-        );
+      const criticalDamage = Math.max(
+        0,
+        Math.floor(
+          entity.accumulatedDamage / (entity.criticalDamageThreshold ?? 2)
+        ) - (entity.criticalDefense ?? 0)
+      );
+      if (criticalDamage > 0) {
+        engine.world.addComponent(entity, "accumulatedCriticalDamage", 0);
+        entity.accumulatedCriticalDamage =
+          (entity.accumulatedCriticalDamage ?? 0) + criticalDamage;
       }
     }
   };
