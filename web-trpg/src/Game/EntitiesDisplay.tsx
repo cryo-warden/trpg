a14302d15d234b2b5f5df7adee94cdbdbc782d05
Cory Watson <cryo.warden@gmail.com>
2025-03-29 01:52:12 -0700

    Fix UI entities sorting

--- web-trpg/src/Game/EntitiesDisplay.tsx
+++ web-trpg/src/Game/EntitiesDisplay.tsx
@@ -2,15 +2,12 @@ import "./EntitiesDisplay.css";
 
 import { useControllerEntity } from "./context/ControllerContext";
 import { EntityPanel } from "./EntityPanel";
-import { Panel } from "../structural/Panel";
-import { useTarget } from "./context/TargetContext";
-import { useMemo } from "react";
 import { useEngine } from "./context/EngineContext";
 import { Entity } from "action-trpg-lib";
 import { Scroller } from "../structural/Scroller";
 
 const weighEntity = (entity: Entity) =>
-  (entity.controller != null ? 1 << 7 : 0) |
+  (entity.controller != null && !entity.unconscious ? 1 << 7 : 0) |
   (entity.path != null ? 1 << 6 : 0) |
   (entity.mhp != null ? 1 << 5 : 0) |
   (entity.contents != null ? 1 << 4 : 0) |
@@ -20,11 +17,9 @@ export const EntitiesDisplay = () => {
   const engine = useEngine();
   const selfEntity = useControllerEntity();
   const entities = selfEntity?.location?.contents ?? [];
-  const sortedEntities = useMemo(() => {
-    return entities
-      .filter((entity) => entity !== selfEntity)
-      .toSorted((a, b) => weighEntity(b) - weighEntity(a));
-  }, [entities]);
+  const sortedEntities = entities
+    .filter((entity) => entity !== selfEntity)
+    .toSorted((a, b) => weighEntity(b) - weighEntity(a));
   return (
     <div className="EntitiesDisplay">
       <Scroller>
