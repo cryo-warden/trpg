242f3be56c6a7e19b1312c9bb3d555c954f1f839
Cory Watson <cryo.warden@gmail.com>
2025-06-06 17:35:05 -0700

    Remove old rendering code

--- web-trpg/src/renderer/debug.tsx
+++ web-trpg/src/renderer/debug.tsx
@@ -9,34 +9,6 @@ import {
 import { EntityId } from "../Game/trpg";
 import { renderTemplate, RenderValue } from "./template";
 
-export const actionWeightType = ["heavy", "neutral", "light"] as const;
-
-export type ActionWeightType = (typeof actionWeightType)[number];
-
-export const actionSpeedType = ["slow", "neutral", "fast"] as const;
-
-export type ActionSpeedType = (typeof actionSpeedType)[number];
-
-export const actionArmamentType = [
-  "blade",
-  "sword",
-  "club",
-  "staff",
-  "fist",
-  "claw",
-  "teeth",
-  "stick",
-  "spout",
-] as const;
-
-export type ActionArmamentType = (typeof actionArmamentType)[number];
-
-export type AttackRenderer = {
-  weightType: ActionWeightType;
-  speedType: ActionSpeedType;
-  armamentType: ActionArmamentType;
-};
-
 export const bindRenderer = ({
   appearanceFeatures,
   appearanceFeaturesComponents,
@@ -182,170 +154,14 @@ export const bindRenderer = ({
     return "hostile";
   };
 
-  const renderSentence = ({
-    viewpointEntity,
-    subject,
-    directObject,
-    indirectObject,
-    verb,
-    particle = "to",
-    finalPunctuation = ".",
-  }: {
-    viewpointEntity: EntityId;
-    subject: EntityId | string;
-    directObject?: EntityId | string;
-    indirectObject?: EntityId | string;
-    verb: string;
-    particle?: string;
-    finalPunctuation?: string;
-  }) => {
-    const subjectName = getName(viewpointEntity, subject) ?? "";
-    const subjectNode = (
-      <span className={getClassName(viewpointEntity, subject)}>
-        {capitalize(subjectName)}
-      </span>
-    );
-
-    const directObjectName = getName(viewpointEntity, directObject, subject);
-    const directObjectNode = (
-      <span className={getClassName(viewpointEntity, directObject)}>
-        {directObjectName}
-      </span>
-    );
-
-    const indirectObjectName = getName(
-      viewpointEntity,
-      indirectObject,
-      subject
-    );
-    const indirectObjectNode = (
-      <span className={getClassName(viewpointEntity, indirectObject)}>
-        {indirectObjectName}
-      </span>
-    );
-
-    if (directObjectName != null && indirectObjectName != null) {
-      return (
-        <div className="debug renderer">
-          {subjectNode} {verb} {directObjectNode} {particle}{" "}
-          {indirectObjectNode}
-          {finalPunctuation}
-        </div>
-      );
-    }
-
-    if (directObjectName != null) {
-      return (
-        <div className="debug renderer">
-          {subjectNode} {verb} {directObjectNode}
-          {finalPunctuation}
-        </div>
-      );
-    }
-
-    if (indirectObjectName != null) {
-      return (
-        <div className="debug renderer">
-          {subjectNode} {verb} {particle} {indirectObjectNode}
-          {finalPunctuation}
-        </div>
-      );
-    }
-
-    return (
-      <div className="debug renderer">
-        {subjectNode} {verb}
-        {finalPunctuation}
-      </div>
-    );
-  };
-
-  const getActionWeightAdjective = (r: AttackRenderer): string => {
-    switch (r.weightType) {
-      case "heavy":
-        return " great";
-      case "neutral":
-        return "";
-      case "light":
-        return " small";
-    }
-  };
-
-  const getActionSpeedVerb = (r: AttackRenderer): string => {
-    switch (r.speedType) {
-      case "slow":
-        return "heave";
-      case "neutral":
-        return "swing";
-      case "fast":
-        return "fling";
-    }
-  };
-
-  const getActionObjectName = (r: AttackRenderer): string => {
-    switch (r.armamentType) {
-      case "blade":
-        return "blade";
-      case "sword":
-        return "sword";
-      case "club":
-        return "club";
-      case "staff":
-        return "staff";
-      case "fist":
-        return "fist";
-      case "claw":
-        return "claw";
-      case "teeth":
-        return "fangs";
-      case "spout":
-        return "spout";
-      case "stick":
-        return "stick";
-    }
-  };
-
-  const getActionDirectObject = (r: AttackRenderer): string => {
-    const verb = getActionSpeedVerb(r);
-    const adjective = getActionWeightAdjective(r);
-
-    switch (r.armamentType) {
-      case "fist":
-        return `${verb} a${adjective} fist`;
-      case "teeth":
-        return `bare${adjective} fangs`;
-      case "spout":
-        return `well up a${adjective} spout`;
-    }
-
-    const objectName = getActionObjectName(r);
-
-    return `${verb} a${adjective} ${objectName}`;
-  };
-
   // WIP Render the new event format.
   const renderAction = (
     viewpointEntity: EntityId,
-    { source, target, action }: any | Extract<EntityEvent, { type: "action" }>
+    { ownerEntityId, targetEntityId }: EntityEvent
   ): ReactNode => {
-    const a = {
-      // WIP
-      name: String(action),
-      renderer: {
-        armamentType: "fist",
-        speedType: "slow",
-        weightType: "heavy",
-      } satisfies AttackRenderer,
-    };
-    return renderSentence({
-      viewpointEntity,
-      subject: source,
-      directObject:
-        a.renderer != null ? getActionDirectObject(a.renderer) : a.name,
-      indirectObject: target,
-      verb: "began to",
-      particle: a.renderer != null ? "at" : "",
-    });
+    return renderWithTemplate("{0:sentence:subject} began to {2} {1:object}.")(
+      viewpointEntity
+    )([ownerEntityId, targetEntityId, "do something to"]);
   };
 
   const renderEvent = (
@@ -360,15 +176,13 @@ export const bindRenderer = ({
           case "Rest":
             return null;
           case "Attack":
-            return renderSentence({
-              viewpointEntity,
-              subject: event.ownerEntityId,
-              directObject: `${event.eventType.value.value} damage`,
-              indirectObject: event.targetEntityId,
-              verb: "dealt",
-              particle: "to",
-              finalPunctuation: "!",
-            });
+            return renderWithTemplate(
+              "{0:sentence:subject} dealt {2} damage to {1:object}!"
+            )(viewpointEntity)([
+              event.ownerEntityId,
+              event.targetEntityId,
+              event.eventType.value.value.toString(),
+            ]);
           // case "dead":
           //   return renderSentence({
           //     viewpointEntity,
@@ -377,21 +191,13 @@ export const bindRenderer = ({
           //     finalPunctuation: "!",
           //   });
           case "Drop":
-            return renderSentence({
-              viewpointEntity,
-              subject: event.ownerEntityId,
-              directObject: event.targetEntityId,
-              verb: "dropped",
-              finalPunctuation: ".",
-            });
+            return renderWithTemplate(
+              "{0:sentence:subject} dropped {1:object}."
+            )(viewpointEntity)([event.ownerEntityId, event.targetEntityId]);
           case "Equip":
-            return renderSentence({
-              viewpointEntity,
-              subject: event.ownerEntityId,
-              directObject: event.targetEntityId,
-              verb: "equipped",
-              finalPunctuation: ".",
-            });
+            return renderWithTemplate(
+              "{0:sentence:subject} equipped {1:object}."
+            )(viewpointEntity)([event.ownerEntityId, event.targetEntityId]);
           case "Heal":
             return renderWithTemplate(
               "{0:sentence:subject} healed {1:object} for {2}."
@@ -415,13 +221,9 @@ export const bindRenderer = ({
           //     finalPunctuation: "!",
           //   });
           case "Take":
-            return renderSentence({
-              viewpointEntity,
-              subject: event.ownerEntityId,
-              directObject: event.targetEntityId,
-              verb: "took",
-              finalPunctuation: ".",
-            });
+            return renderWithTemplate("{0:sentence:subject} took {1:object}.")(
+              viewpointEntity
+            )([event.ownerEntityId, event.targetEntityId]);
           // case "unconscious":
           //   return renderSentence({
           //     viewpointEntity,
@@ -430,13 +232,9 @@ export const bindRenderer = ({
           //     finalPunctuation: "!",
           //   });
           case "Unequip":
-            return renderSentence({
-              viewpointEntity,
-              subject: event.ownerEntityId,
-              directObject: event.targetEntityId,
-              verb: "unequipped",
-              finalPunctuation: ".",
-            });
+            return renderWithTemplate(
+              "{0:sentence:subject} unequipped {1:object}."
+            )(viewpointEntity)([event.ownerEntityId, event.targetEntityId]);
         }
     }
 
