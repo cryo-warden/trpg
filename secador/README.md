09d032d29f8ef811cc8ba6918f2317dedf913e99
Cory Watson <cryo.warden@gmail.com>
2025-08-29 22:46:35 -0700

    Update secador docs

---
+++ secador/README.md
@@ -0,0 +1,3 @@
+Stop repeating code.
+
+(Unless you really should because it's only the same by coincidence.)
--- secador/src/lib.rs
+++ secador/src/lib.rs
@@ -11,16 +11,14 @@
 //!
 //! # Example
 //! ```rust
-//! use secador::secador;
-//!
-//! # struct SomeContainer;
-//! # impl SomeContainer {
-//! #     fn some_method(&self) -> Vec<i32> { vec![1, 2, 3] }
-//! #     fn another_method(&self) -> Vec<i32> { vec![4, 5, 6] }
-//! # }
-//! # let some_container = SomeContainer;
-//! #
-//! secador!((variable_iter), [(some_method), (another_method)], {
+//! secador::secador!((variable_iter), [(some_method), (another_method)], {
+//!     # struct SomeContainer;
+//!     # impl SomeContainer {
+//!     #     fn some_method(&self) -> Vec<i32> { vec![1, 2, 3] }
+//!     #     fn another_method(&self) -> Vec<i32> { vec![4, 5, 6] }
+//!     # }
+//!     # let some_container = SomeContainer;
+//!     #
 //!     seca!(1);
 //!     for x in some_container.__variable_iter().iter() {
 //!         println!("{:?}", x);
@@ -48,12 +46,20 @@ mod tests;
 
 /// A macro to reduce boilerplate code in Rust.
 /// Takes a tuple of names, a list of substition tuples, and a block of code.
-/// The block of code is duplicated for each substitution tuple, with each
-/// name replaced by the corresponding value in the tuple.
+/// Each section marked with `seca!` in that block of code is duplicated for
+/// each substitution tuple, with each name (expressed as `__name`) replaced
+/// by the corresponding value in the tuple.
+///
+/// When using `seca!` to denote statements, in the parentheses include the
+/// number of following statements to duplicate. Example: `seca!(2);` for
+/// two statements.
+///
+/// When using `__seca` to denote fields, instead of a value, write the number
+/// of following field lines to duplicate with a leading double underscore.
+/// Example: `__seca: __3,` for three fields.
+///
 /// # Example
 /// ```rust
-/// use secador::secador;
-///
 /// # struct SomeContainer;
 /// # impl SomeContainer {
 /// #     fn some_method(&self) -> Vec<i32> { vec![1, 2, 3] }
@@ -61,7 +67,7 @@ mod tests;
 /// # }
 /// # let some_container = SomeContainer;
 /// #
-/// secador!((variable_iter), [(some_method), (another_method)], {
+/// secador::secador!((variable_iter), [(some_method), (another_method)], {
 ///     seca!(1);
 ///     for x in some_container.__variable_iter().iter() {
 ///         println!("{:?}", x);
@@ -75,23 +81,21 @@ pub use secador_macro::secador;
 ///
 /// # Example
 /// ```rust
-/// use secador::secador_multi;
-///
-/// # struct SomeContainer;
-/// # impl SomeContainer {
-/// #     fn some_method(&self) -> Vec<i32> { vec![1, 2, 3] }
-/// #     fn another_method(&self) -> Vec<i32> { vec![4, 5, 6] }
-/// # }
-/// # let some_container = SomeContainer;
-/// #
-/// secador_multi!(
+/// secador::secador_multi!(
 ///   seca_iter!((variable_iter), [(some_method), (another_method)]),
 ///   seca_attr!((attribute), [(derive(Debug)), (derive(Clone))]),
 ///   {
+///     # struct SomeContainer;
+///     # impl SomeContainer {
+///     #     fn some_method(&self) -> Vec<i32> { vec![1, 2, 3] }
+///     #     fn another_method(&self) -> Vec<i32> { vec![4, 5, 6] }
+///     # }
+///     # let some_container = SomeContainer;
+///     #
 ///     #[seca_attr(1)]
 ///     #[__attribute]
 ///     pub struct SomeStruct(pub i32);
-
+///
 ///     seca_iter!(1);
 ///     for x in some_container.__variable_iter().iter() {
 ///       println!("{:?}", x);
